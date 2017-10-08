a27cbb2121cc657fa800d7b670c226b35dc40249
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-10-08 11:08:10 -0400

    Adoptioni Added AssemblyVersion Recompiled for 1.3.1

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+
+@echo off
+
+set H=R:\KSP_1.3.1_dev
+set GAMEDIR=RCSBuildAid
+
+echo %H%
+
+copy /Y "%1%2" "GameData\%GAMEDIR%\Plugins"
+
+
+
+mkdir "%H%\GameData\%GAMEDIR%"
+xcopy  /E /y GameData\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
